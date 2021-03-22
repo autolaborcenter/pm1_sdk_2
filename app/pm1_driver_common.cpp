5257f04ada531bc466a44858628ed0bad2159281
YdrMaster <ydrml@hotmail.com>
2021-03-22 15:54:11 +0800

    refactor(app): reformat

--- app/pm1_driver_common.cpp
+++ app/pm1_driver_common.cpp
@@ -6,8 +6,7 @@
 #include <cmath>
 #include <iostream>
 
-namespace autolabor::pm1
-{
+namespace autolabor::pm1 {
     loop_msg_t::loop_msg_t() : _bytes{} {
         #define SET_HEADER(N) *reinterpret_cast<can::header_t *>(_bytes + (N)) = can::pm1
         SET_HEADER(0)::every_tcu::current_position::tx;
@@ -36,102 +35,84 @@ namespace autolabor::pm1
         std::memcpy(buffer, _bytes, size);
         return {buffer, size};
     }
-
-
-    std::thread launch_parser(std::mutex &mutex, std::condition_variable &signal, std::unordered_map<std::string, chassis_t> &chassis)
-    {
+    
+    
+    std::thread launch_parser(std::mutex &mutex, std::condition_variable &signal, std::unordered_map<std::string, chassis_t> &chassis) {
         return std::thread([&mutex, &signal, &chassis] {
             using namespace std::chrono;
             using namespace std::chrono_literals;
-
+            
             std::string target, temp;
             float v = NAN;
             steady_clock::time_point t0;
-
-            enum class STATE
-            {
-                IDLE,
-                V,
-                B,
-            } state = STATE::IDLE;
-
-            while (std::cin >> temp)
-            {
-                switch (state)
-                {
-                case STATE::IDLE:
-                    if (temp.size() == 1)
-                        switch (temp[0])
-                        {
-                        case 'N':
-                        {
+            
+            enum class STATE { IDLE, V, B, } state = STATE::IDLE;
+            
+            while (std::cin >> temp) {
+                switch (state) {
+                    case STATE::IDLE:
+                        if (temp.size() == 1)
+                            switch (temp[0]) {
+                                case 'N': {
+                                    std::unique_lock<std::mutex> lock(mutex);
+                                    for (const auto &[name, _] : chassis)
+                                        std::cout << name << ' ';
+                                    std::cout << std::endl;
+                                }
+                                    break;
+                                case 'V':
+                                    t0 = steady_clock::now();
+                                    state = STATE::V;
+                                    break;
+                                case 'B':
+                                    t0 = steady_clock::now();
+                                    state = STATE::B;
+                                    break;
+                            }
+                        break;
+                    case STATE::V: { // velocity
+                        if (steady_clock::now() > t0 + 20ms) {
+                            state = STATE::IDLE;
+                            break;
+                        }
+                        if (target.empty()) {
+                            target = std::move(temp);
+                            break;
+                        }
+                        float value;
+                        try {
+                            value = std::stof(temp);
+                        }
+                        catch (...) {
+                            state = STATE::IDLE;
+                            break;
+                        }
+                        if (std::isinf(value)) {
+                            state = STATE::IDLE;
+                            break;
+                        }
+                        if (std::isnan(v))
+                            v = value;
+                        else {
                             std::unique_lock<std::mutex> lock(mutex);
-                            for (const auto &[name, _] : chassis)
-                                std::cout << name << ' ';
-                            std::cout << std::endl;
+                            auto p = chassis.find(target);
+                            if (p != chassis.end())
+                                p->second.set_velocity(v, value);
+                            lock.unlock();
+                            target.clear();
+                            v = NAN;
+                            state = STATE::IDLE;
                         }
                         break;
-                        case 'V':
-                            t0 = steady_clock::now();
-                            state = STATE::V;
-                            break;
-                        case 'B':
-                            t0 = steady_clock::now();
-                            state = STATE::B;
-                            break;
-                        }
-                    break;
-                case STATE::V:
-                { // velocity
-                    if (steady_clock::now() > t0 + 20ms)
-                    {
-                        state = STATE::IDLE;
-                        break;
                     }
-                    if (target.empty())
-                    {
-                        target = std::move(temp);
-                        break;
-                    }
-                    float value;
-                    try
-                    {
-                        value = std::stof(temp);
-                    }
-                    catch (...)
-                    {
-                        state = STATE::IDLE;
-                        break;
-                    }
-                    if (std::isinf(value))
-                    {
-                        state = STATE::IDLE;
-                        break;
-                    }
-                    if (std::isnan(v))
-                        v = value;
-                    else
-                    {
+                    case STATE::B: { // battery
                         std::unique_lock<std::mutex> lock(mutex);
-                        auto p = chassis.find(target);
+                        auto p = chassis.find(temp);
                         if (p != chassis.end())
-                            p->second.set_velocity(v, value);
-                        lock.unlock();
-                        target.clear();
-                        v = NAN;
+                            std::cout << +p->second.battery_percent() << std::endl;
                         state = STATE::IDLE;
+                        break;
                     }
-                    break;
-                }
-                case STATE::B:
-                { // battery
-                    std::unique_lock<std::mutex> lock(mutex);
-                    auto p = chassis.find(temp);
-                    if (p != chassis.end())
-                        std::cout << +p->second.battery_percent() << std::endl;
-                    state = STATE::IDLE;
-                    break;
-                }
                 }
             }
             {
--- app/pm1_driver_common.h
+++ app/pm1_driver_common.h
@@ -9,23 +9,21 @@
 #include <thread>
 #include <condition_variable>
 
-namespace autolabor::pm1
-{
+namespace autolabor::pm1 {
     // 应该使用一个线程循环发送的询问信息
     // 基于一个基本周期（40 ms），询问后轮角度、前轮编码器、节点状态和电池电量
-
-    class loop_msg_t
-    {
+    
+    class loop_msg_t {
         uint8_t _bytes[4 * 6];
-
+    
     public:
         constexpr static auto PERIOD = std::chrono::milliseconds(40);
-
+        
         loop_msg_t();
-
+        
         std::pair<uint8_t *, size_t> operator[](uint64_t) const;
     };
-
+    
     std::thread launch_parser(std::mutex &, std::condition_variable &, std::unordered_map<std::string, chassis_t> &);
 }
 
