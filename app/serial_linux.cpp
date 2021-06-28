b46ff3f58b6e508eba84cc40332989aa9980606b
YdrMaster <ydrml@hotmail.com>
2021-06-28 22:10:07 +0800

    todo: test flock

--- app/serial_linux.cpp
+++ app/serial_linux.cpp
@@ -1,8 +1,9 @@
 ﻿#include "serial_linux.h"
 
-#include <fcntl.h>  // open
-#include <termios.h>// config
-#include <unistd.h> // close
+#include <fcntl.h>   // open
+#include <sys/file.h>// flock
+#include <termios.h> // config
+#include <unistd.h>  // close
 
 #include <filesystem>
 
@@ -19,8 +20,13 @@ std::vector<std::string> list_ports() {
 
 int open_serial(std::string const &name) {
     std::filesystem::path path("/dev");
-    auto fd = open(path.append(name).c_str(), O_RDWR);
+    auto fd = open(path.append(name).c_str(), O_RDWR | O_NOCTTY);
     if (fd < 0) return fd;
+
+    if (flock(fd, LOCK_EX | LOCK_NB)) {
+        close(fd);
+        return -1;
+    }
 
     // @see https://blog.mbedded.ninja/programming/operating-systems/linux/linux-serial-ports-using-c-cpp/
     termios tty{};
