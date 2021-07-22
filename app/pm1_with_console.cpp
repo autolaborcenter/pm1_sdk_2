3379b27c3ddf7ff86d7245631a3fa38e613938ae
YdrMaster <ydrml@hotmail.com>
2021-07-22 17:42:24 +0800

    refactor: 交互无状态化

--- app/pm1_with_console.cpp
+++ app/pm1_with_console.cpp
@@ -64,7 +64,7 @@ int main() {
                         // 路径 := 间隔 0.1s 的 15 个点
                         // 0.1s x 15 = 1.5s > 1.45s = 1.25s + 200ms
                         // [P]ath
-                        std::cout << 'P';
+                        std::cout << "P " << target.speed << '|' << target.rudder;
                         autolabor::odometry_t<> pose{};
                         for (auto i = 0; i < 15; ++i) {
                             for (auto j = 0; j < 5; ++j)
